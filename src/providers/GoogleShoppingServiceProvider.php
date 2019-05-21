c26e05634622c421692720f15aa80410c90cf461
haroonkhan <haroon.khan@nxb.com.pk>
2019-05-21 14:17:47 +0500

    First Commit

---
+++ src/providers/GoogleShoppingServiceProvider.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Hkonnet\LaravelGoogleShopping\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class GoogleShoppingServiceProvider extends ServiceProvider {
+    public function boot(){
+        
+    }
+
+    public function register(){
+
+    }
+}
